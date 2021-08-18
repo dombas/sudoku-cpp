df9b8d3a2adb8116e4f7e2cc1f450c37b659ddf9
Dominik Dąbek <dominikdabek08@gmail.com>
2021-08-18 20:06:29 +0200

    Add .clang-format and reformat code

--- SudokuConsoleUi/SudokuCommand.h
+++ SudokuConsoleUi/SudokuCommand.h
@@ -1,25 +1,25 @@
 #pragma once
-#include "../SudokuLib/Board.h"
 #include <string>
+
+#include "../SudokuLib/Board.h"
 
 using std::string;
 
 namespace Sudoku {
-	class SudokuCommand
-	{
-	protected:
-		const string input;
-		string message;
-		bool valid;
-		board_index_datatype row, column;
-		field_datatype value;
+class SudokuCommand {
+ protected:
+  const string input;
+  string message;
+  bool valid;
+  board_index_datatype row, column;
+  field_datatype value;
 
-		void parse_input();
-	public:
-		SudokuCommand(string input);
-		bool is_valid();
-		string get_message();
-		void apply_to_board(Board& board);
-	};
-} // Sudoku
+  void parse_input();
 
+ public:
+  SudokuCommand(string input);
+  bool is_valid();
+  string get_message();
+  void apply_to_board(Board& board);
+};
+}  // namespace Sudoku
